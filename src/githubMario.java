13dd612e10410c36348770ed80da7a4a391040f5
mariodimas <mario20ti@mahasiswa.pcr.ac.id>
2021-07-02 21:54:39 +0700

    Revert "Hola Mundo"
    This reverts commit d95e7e1a201147ff12313b5f135536086cb66489.

--- src/githubMario.java
+++ src/githubMario.java
@@ -12,6 +12,5 @@ public class githubMario {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hallo Dunia");
-        System.out.println("Hola Mundo");
     }
 }

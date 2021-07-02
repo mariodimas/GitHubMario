d95e7e1a201147ff12313b5f135536086cb66489
mariodimas <mario20ti@mahasiswa.pcr.ac.id>
2021-07-02 21:46:48 +0700

    Hola Mundo

--- src/githubMario.java
+++ src/githubMario.java
@@ -12,5 +12,6 @@ public class githubMario {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hallo Dunia");
+        System.out.println("Hola Mundo");
     }
 }
